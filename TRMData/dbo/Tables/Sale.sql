bda4b328ebd08d7c579fd1f3bcca5c61c5ca2f37
Vince Catalano <vince.catalano@magenium.com>
2021-04-11 09:23:24 -0500

    Register DB Tables
    The tables for the basic register ssytem.

---
+++ TRMData/dbo/Tables/Sale.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Sale]
+(
+	[Id] INT NOT NULL PRIMARY KEY identity, 
+    [CashierId] NVARCHAR(128) NOT NULL, 
+    [SalesDate] DATETIME2 NOT NULL, 
+    [SubTotal] MONEY NOT NULL, 
+    [Tax] MONEY NOT NULL, 
+    [Total] MONEY NOT NULL
+)
---
+++ TRMData/dbo/Tables/User.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[User]
+(	
+    [Id] NvarCHAR(128) NOT NULL primary key, 
+    [FirstName] NvarCHAR(50) NOT NULL, 
+    [LastName] NvarCHAR(50) NOT NULL, 
+    [EmailAddress] NvarCHAR(256) NOT NULL, 
+    [CreatedDate] DATETIME2 NOT NULL DEFAULT getutcdate()
+)
