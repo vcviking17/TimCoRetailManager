b0b2e91546dc17fd0193043212b1576c8fb7ee58
Vince Catalano <vince.catalano@magenium.com>
2021-05-14 09:24:06 -0500

    Moved Secure Key to Configuration (Appsettings)

--- TRMApi/Startup.cs
+++ TRMApi/Startup.cs
@@ -62,7 +62,8 @@ namespace TRMApi
                     JwtBearerOptions.TokenValidationParameters = new TokenValidationParameters
                     {
                         ValidateIssuerSigningKey = true,
-                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("MySecretKeyIsSecretSoDoNotTell")),
+                        //IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("MySecretKeyIsSecretSoDoNotTell")),
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration.GetValue<string>("Secrets:SecurityKey"))),
                         ValidateIssuer = false,
                         ValidateAudience = false,
                         ValidateLifetime = true,
